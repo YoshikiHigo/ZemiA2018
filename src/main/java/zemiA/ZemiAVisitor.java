85737808bfee06a911daa2a6148db61455af4c57
anonymous <anonymous@DESKTOP-7EQQVA7>
2018-06-29 14:26:56 +0900

    LOCの実装，コンストラクタの変更

--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -2,9 +2,6 @@ package zemiA;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.PrimitiveType;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class ZemiAVisitor extends ASTVisitor {
@@ -13,25 +10,11 @@ public class ZemiAVisitor extends ASTVisitor {
 		this.unit = compilationUnit;
 	}
 
-	@Override
-	public boolean visit(SimpleName node) {
-		//System.out.println(node.getIdentifier());
-		return super.visit(node);
-	}
-
   @Override
   public boolean visit(MethodDeclaration node) {
     System.out.print(node.getName() + " method LOC = ");
     System.out.println(unit.getLineNumber(node.getStartPosition() + node.getLength() -1)+1-unit.getLineNumber(node.getStartPosition()));
     return super.visit(node);
   }
-
-  @Override
-  public boolean visit(PrimitiveType node) {
-    // TODO Auto-generated method stub
-    return super.visit(node);
-  }
-
-	
 	
 }
