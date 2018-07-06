6cd65e4fddd1b41e5abe97aa68eb188f3d10b466
riku Iwamoto <u534048d@ecs.osaka-u.ac.jp>
2018-07-06 13:56:53 +0900

    NOPAを追加

--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -1,12 +1,15 @@
 package zemiA;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.PrimitiveType;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 
 public class ZemiAVisitor extends ASTVisitor {
+
+	private int privatefields;
 
 	@Override
 	public boolean visit(SimpleName node) {
@@ -14,18 +17,29 @@ public class ZemiAVisitor extends ASTVisitor {
 		return super.visit(node);
 	}
 
-  @Override
-  public boolean visit(MethodDeclaration node) {
-    System.out.println(node.toString());
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		System.out.println(node.toString());
+		return super.visit(node);
+	}
 
-  @Override
-  public boolean visit(PrimitiveType node) {
-    // TODO Auto-generated method stub
-    return super.visit(node);
-  }
+	@Override
+	public boolean visit(PrimitiveType node) {
+		// TODO Auto-generated method stub
+		return super.visit(node);
+	}
 
-	
-	
+	//NOPA Number of public attributes /Entity: Class
+	//public修飾子のついたfield変数の数
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		System.out.print(node.toString());
+		if(Modifier.isPublic(node.getModifiers())) {
+			this.privatefields+=1;
+			System.out.println("isPublic");
+		}
+		System.out.println(this.privatefields);
+		return super.visit(node);
+	}
+
 }
