2d16de706bfb3e0af061587ae22e2d1d555209ba
riku Iwamoto <u534048d@ecs.osaka-u.ac.jp>
2018-07-11 14:53:26 +0900

    accessor methodのListを作成

--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -1,17 +1,26 @@
 package zemiA;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class ZemiAVisitor extends ASTVisitor {
 
 	private int privatefields;
   CompilationUnit unit;
+  private String methodname;
+  private Map<String,Boolean> fieldmap= new HashMap<String,Boolean>();
+  private ArrayList<String> methodlist = new ArrayList<String>();
+  private Map<String,Boolean> accessormethodmap= new HashMap<String,Boolean>();
 	public ZemiAVisitor(CompilationUnit compilationUnit) {
 		this.unit = compilationUnit;
 	}
@@ -26,6 +35,12 @@ public class ZemiAVisitor extends ASTVisitor {
   public boolean visit(MethodDeclaration node) {
     System.out.print(node.getName() + " method LOC = ");
     System.out.println(unit.getLineNumber(node.getStartPosition() + node.getLength() -1)+1-unit.getLineNumber(node.getStartPosition()));
+    methodname = node.getName().toString();
+	accessormethodmap.put(methodname, Boolean.FALSE);
+	methodlist.add(methodname);
+	System.out.println("*"+node.getName()+"*");
+	System.out.println(node.resolveBinding().getDeclaringClass());
+
     return super.visit(node);
   }
 
@@ -47,5 +62,21 @@ public class ZemiAVisitor extends ASTVisitor {
 		System.out.println(this.privatefields);
 		return super.visit(node);
 	}
+
+	@Override
+	public boolean visit(ReturnStatement node) {
+		System.out.println(node.getExpression());
+		if(node.getExpression().toString().startsWith("this")) {
+			System.out.println("getter method");
+			accessormethodmap.replace(methodname, Boolean.TRUE);
+		}else if(fieldmap.get(node.getExpression().toString())==null) {
+			System.out.println("normal method");
+		}else if(fieldmap.get(node.getExpression().toString())) {
+			System.out.println("*"+"getter method"+"*");
+			accessormethodmap.replace(methodname, Boolean.TRUE);
+		}
+		return super.visit(node);
+	}
+
 
 }
